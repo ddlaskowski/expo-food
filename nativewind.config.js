f13c58525e7280649053e75a81a4d2df5b512579
Damian <dd.laskowski@gmail.com>
2025-07-09 19:40:18 +0200

    React Native with Expo Food delivery app

---
+++ nativewind.config.js
@@ -0,0 +1,14 @@
+// nativewind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        "quicksand": ["Quicksand-Regular"],
+        "quicksand-bold": ["Quicksand-Bold"],
+        "quicksand-medium": ["Quicksand-Medium"],
+        "quicksand-light": ["Quicksand-Light"],
+        "quicksand-semibold": ["Quicksand-SemiBold"],
+      },
+    },
+  },
+};
